1096390b8906d0395d01bed6c9a2d61b80463601
LuisVitorVaz <luis-mattos@uergs.edu.br>
2025-06-17 08:37:15 -0300

    Versao derivada do codigo

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-#include <math.h> // Necessário para usar sqrt() e acos()
+#include <math.h> // Para sqrt() e acos()
 
 #define SENSOR1 A0
 #define SENSOR2 A1
@@ -15,44 +15,15 @@
 #define LED_DIREITA 9
 #define LED_FRENTE 10
 #define LED_ATRAS 11
-
-float theta = 0;
-double media_valores1[10] = {0};
-double media_valores2[10] = {0};
 int valor1 = 0;
 int valor2 = 0;
+float theta = 0;
+double media_valores1[20] = {0};
+double media_valores2[20] = {0};
 int cont = 0;
+
 double max1 = 0;
 double max2 = 0;
-
-void funcao_media() {
-  if (cont == 10) {
-    // Reset dos máximos
-    max1 = media_valores1[0];
-    max2 = media_valores2[0];
-
-    for (int i = 1; i < 10; i++) {
-      if (media_valores1[i] > max1) {
-        max1 = media_valores1[i];
-      }
-      if (media_valores2[i] > max2) {
-        max2 = media_valores2[i];
-      }
-
-      Serial.print("Vetor1[");
-      Serial.print(i);
-      Serial.print("] = ");
-      Serial.println(media_valores1[i]);
-    }
-
-    Serial.print("Maior valor vetor1: ");
-    Serial.println(max1);
-    Serial.print("Maior valor vetor2: ");
-    Serial.println(max2);
-
-    cont = 0; // reinicia contador
-  }
-}
 
 void apaga_leds() {
   digitalWrite(LED_ESQUERDA, LOW);
@@ -65,39 +36,64 @@ void apaga_leds() {
   digitalWrite(LED_45_DIR_TRAS, LOW);
 }
 
-void angulo_theta() {
+void detectar_picos() {
+  // Detecta pico local baseado em mudança de sinal na derivada
+  max1 = media_valores1[0];
+  max2 = media_valores2[0];
+
+  for (int i = 1; i < 19; i++) {
+    double d1 = media_valores1[i] - media_valores1[i - 1];
+    double d2 = media_valores1[i + 1] - media_valores1[i];
+    if (d1 > 0 && d2 < 0 && media_valores1[i] > max1) {
+      max1 = media_valores1[i];
+    }
+
+    d1 = media_valores2[i] - media_valores2[i - 1];
+    d2 = media_valores2[i + 1] - media_valores2[i];
+    if (d1 > 0 && d2 < 0 && media_valores2[i] > max2) {
+      max2 = media_valores2[i];
+    }
+  }
+
+  Serial.print("Pico detectado Sensor 1: ");
+  delay(100);
+  Serial.println(max1);
+  Serial.print("Pico detectado Sensor 2: ");
+  delay(100);
+  Serial.println(max2);
+}
+
+void calcular_theta_e_acender_led() {
   apaga_leds();
 
   if (max1 > 0 && max2 > 0) {
-    double hipotenusa = sqrt(pow(max1, 2) + pow(max2, 2));
-    theta = acos(max1 / hipotenusa); // theta em radianos
+    theta = atan2(max2, max1);  // agora theta considera os 2 quadrantes
+    if (theta < 0) theta += 2 * PI;
+
     Serial.print("Theta (radianos): ");
     Serial.println(theta);
 
-    // Mapeamento dos ângulos para os LEDs
-    if (theta < 0.4) {
-      digitalWrite(LED_45_ESQ_TRAS, HIGH);
-    } else if (theta < 0.6) {
-      digitalWrite(LED_ESQUERDA, HIGH);
-    } else if (theta < 0.8) {
-      digitalWrite(LED_45_ESQ_FRENTE, HIGH);
-    } else if (theta < 1.2) {
+    // Mapeamento com base no círculo
+    if (theta < PI / 8 || theta > 15 * PI / 8) {
       digitalWrite(LED_FRENTE, HIGH);
-    } else if (theta < 1.4) {
+    } else if (theta < 3 * PI / 8) {
       digitalWrite(LED_45_DIR_FRENTE, HIGH);
-    } else if (theta < 1.6) {
+    } else if (theta < 5 * PI / 8) {
       digitalWrite(LED_DIREITA, HIGH);
-    } else if (theta < 1.8) {
+    } else if (theta < 7 * PI / 8) {
       digitalWrite(LED_45_DIR_TRAS, HIGH);
-    } else {
+    } else if (theta < 9 * PI / 8) {
       digitalWrite(LED_ATRAS, HIGH);
+    } else if (theta < 11 * PI / 8) {
+      digitalWrite(LED_45_ESQ_TRAS, HIGH);
+    } else if (theta < 13 * PI / 8) {
+      digitalWrite(LED_ESQUERDA, HIGH);
+    } else {
+      digitalWrite(LED_45_ESQ_FRENTE, HIGH);
     }
-
   } else {
     Serial.println("Erro: valores inválidos para cálculo de theta");
   }
-
-  delay(500); // Aguarda meio segundo
 }
 
 void setup() {
@@ -114,21 +110,23 @@ void setup() {
 }
 
 void loop() {
-  valor1 = analogRead(SENSOR1);
-  valor2 = analogRead(SENSOR2);
-
-  Serial.print("Sensor 1: ");
-  Serial.println(valor1);
-  Serial.print("Sensor 2: ");
-  Serial.println(valor2);
+   valor1 = analogRead(SENSOR1);
+   valor2 = analogRead(SENSOR2);
 
   media_valores1[cont] = valor1;
   media_valores2[cont] = valor2;
   cont++;
-
-  Serial.print("Contador: ");
+  Serial.println("vslor do cont");
   Serial.println(cont);
 
-  funcao_media();
-  angulo_theta();
+  if (cont == 20) {
+    detectar_picos();
+    calcular_theta_e_acender_led();
+
+    cont = 0;
+    max1 = 0;
+    max2 = 0;
+  }
+
+  delay(2); // Pequeno delay para taxa de aquisição estável
 }
